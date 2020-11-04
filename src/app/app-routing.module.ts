2a281bb91a87d0068ecc7fedbccae71605a9d008
Andrés Barreto <omar@150porciento.com>
2020-11-03 19:08:45 -0500

    add rounting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomepageComponent
+  },
+  {
+    path:'**',
+    redirectTo:'/'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
